7bf98841dac975ea301ad3a4d7ca7147d8abbec5
Nabil Ridhwan <nabridhwan@gmail.com>
2024-10-22 16:15:43 +0800

    feat(widget): add base widget

---
+++ CountdownWidget/CountdownWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownWidgetBundle.swift
+//  CountdownWidget
+//
+//  Created by Nabil Ridhwan on 22/10/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CountdownWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CountdownWidget()
+        CountdownWidgetControl()
+        CountdownWidgetLiveActivity()
+    }
+}
